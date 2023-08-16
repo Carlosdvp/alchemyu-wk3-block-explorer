952fcc4864443d5e3a70a2f9a36fe49a8fb6b994
Carlosdvp <carlosdvp.tx@gmail.com>
2023-08-16 12:46:22 -0400

    finished implementing the logic and adding the button to reset the transactions display back to the first 10.

--- src/components/BlockDetails.jsx
+++ src/components/BlockDetails.jsx
@@ -5,7 +5,6 @@ const settings = {
   apiKey: process.env.REACT_APP_ALCHEMY_API_KEY,
   network: Network.ETH_MAINNET,
 };
-
 const alchemy = new Alchemy(settings);
 
 function displayTransactions(transactions, startIndex) {
@@ -17,14 +16,15 @@ function displayTransactions(transactions, startIndex) {
 export const BlockDetails = () => {
   const [blockNumber, setBlockNumber] = useState();
   const [block, setBlock] = useState();
-  const [startIndex, setStartIndex] = useState(0); // New state to track index
+  const [startIndex, setStartIndex] = useState(0);
+  const [resetTxCount, setResetTxCount] = useState(false);
 
   useEffect(() => {
     async function fetchData() {
       try {
         const latestBlockNumber = await alchemy.core.getBlockNumber();
         setBlockNumber(latestBlockNumber);
-        
+
         const latestBlock = await alchemy.core.getBlock(latestBlockNumber)
         setBlock(latestBlock)
       } catch (error) {
@@ -39,6 +39,18 @@ export const BlockDetails = () => {
     setStartIndex(prevIndex => prevIndex + 10);
   };
 
+  const handleResetButtonClick = () => {
+    setStartIndex(0);
+    setResetTxCount(true);
+  }
+
+  useEffect(() => {
+    if (resetTxCount) {
+      setStartIndex(0);
+      setResetTxCount(false);
+    }
+  }, [resetTxCount])
+
   return (
     <div className='bg-slate-300 h-full'>
       <h1 className='text-red-900 font-semibold text-3xl text-center py-12'>
@@ -52,13 +64,27 @@ export const BlockDetails = () => {
         className='px-6'>
         {displayTransactions(block?.transactions || [], startIndex)}
       </div>
-      <div className='flex flex-col w-[30%] items-center pt-12 mx-auto my-0'>
-        <label className='mb-2'>Show next 10 Transactions</label>
-        <button
-          className='bg-slate-600 text-white hover:bg-slate-200 hover:text-slate-700 py-2 px-4 border border-red-950 rounded-md w-[50%]'
-          onClick={handleNextButtonClick}>
-          Next 10
-        </button>
+      <div className='flex flex-row w-[50%] items-center pt-12 mx-auto my-0'>
+        <div className='flex flex-col w-[45%] items-center'>
+          <label className='mb-2'>
+            Show next 10 Transactions
+          </label>
+          <button
+            className='bg-slate-600 text-white hover:bg-slate-200 hover:text-slate-700 py-2 px-4 border border-red-950 rounded-md w-[50%]'
+            onClick={handleNextButtonClick}>
+            Next 10
+          </button>  
+        </div>
+        <div className='flex flex-col w-[45%] items-center'>
+          <label className='mb-2'>
+            Reset Transactions Display
+          </label>
+          <button
+            className='bg-slate-600 text-white hover:bg-slate-200 hover:text-slate-700 py-2 px-4 border border-red-950 rounded-md w-[50%]'
+            onClick={handleResetButtonClick}>
+            Reset
+          </button>
+        </div>
       </div>
     </div>
   )
